1268d5781b8cdab38b3431b7ddf6de5f6c3c7fe5
Edward Litvinchuck <elitvinchuck@gmail.com>
2024-06-16 18:55:36 +0300

    Added alembic and added users migrations

--- src/models.py
+++ src/models.py
@@ -1,7 +1,8 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Date
 from sqlalchemy.orm import relationship
+from sqlalchemy.orm import declarative_base
 
-from src.configuration.database import Base
+Base = declarative_base()
 
 # User (id, name, birhtday)
 # https://docs.sqlalchemy.org/en/13/core/type_basics.html#generic-types
@@ -9,6 +10,6 @@ class User(Base):
     __tablename__="users"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String, null=False)
-    birthday = Column(Date, null=False)
+    name = Column(String, nullable=False)
+    birthday = Column(Date, nullable=False)
 
