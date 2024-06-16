94d88a23f20cf03fb8eec919fbf73cf220be3159
Edward Litvinchuck <elitvinchuck@gmail.com>
2024-06-16 19:13:54 +0300

    Create models Workout, Exercise, Workout_exercise

--- src/models.py
+++ src/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Date
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Date, Time
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import declarative_base
 
@@ -13,3 +13,25 @@ class User(Base):
     name = Column(String, nullable=False)
     birthday = Column(Date, nullable=False)
 
+class Workout(Base):
+    __tablename__= "workout"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+
+class Exercise(Base):
+    __tablename__ = "exercise"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    duration = Column(Time)
+    calories = Column(Integer)
+
+class Workout_exercise(Base):
+    __tablename__ = 'workout_exercise'
+
+    id = Column(Integer, primary_key=True)
+    workout_id = Column(Integer, ForeignKey('workout.id'))
+    exercise_id = Column(Integer, ForeignKey('exercise.id'))
+    number = Column(Integer)
